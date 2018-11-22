0ead4e6ce7034cda7cf1788be4b6c10fb8edeb39
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-22 11:06:58 -0600

    updated files to include export statements

--- src/app/App.js
+++ src/app/App.js
@@ -1,34 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from '../Components/Header/Header';
+import Player from '../Components/Player/Player';
 
 
 
 class App extends Component {
   render () {
     <Header />,
-    <Player />,
-    <Counter />
+    <Player />
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button className="btn btn-danger">ImaButton</button>
-
-        </header>
+      <div className="scoreboard">
+      <Header />
+      <Player />
       </div>
+
     );
   }
 }
