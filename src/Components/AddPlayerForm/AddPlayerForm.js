4ea637409c37a8b23821f60eb8011a7558593f20
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-25 17:30:32 -0600

    controlled input in place to manually manage the state of the the addPlayerForm component

--- src/Components/AddPlayerForm/AddPlayerForm.js
+++ src/Components/AddPlayerForm/AddPlayerForm.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class AddPlayerForm extends React.Component {
+
+  state = {
+    value: ''
+  }
+
+  handleValueChange = (e) => {
+    this.setState({ value: e.target.value});
+  }
+
   render() {
+    console.log(this.state.value);
     return (
       <form>
         <input
           type="text"
+          value={this.state.value}
+          onChange={this.handleValueChange}
           placeholder="Enter a player's name"
         />
         <input
