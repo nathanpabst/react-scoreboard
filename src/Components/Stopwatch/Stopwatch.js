f4b857ed6faf710fb511ae9c177944f6cdfe473f
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-26 12:29:00 -0600

    added event handler to the stopwatch.

--- src/Components/Stopwatch/Stopwatch.js
+++ src/Components/Stopwatch/Stopwatch.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 class Stopwatch extends React.Component {
+
+  state = {
+    isRunning: false
+  };
+
+  handleStopwatch = () => {
+    this.setState({
+      isRunning: !this.state.isRunning
+    });
+  }
+
   render() {
     return(
-      <div>
+      <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={this.handleStopwatch}>{ this.state.isRunning ? 'Stop' : 'Start' }</button>
         <button>Reset</button>
       </div>
     );
